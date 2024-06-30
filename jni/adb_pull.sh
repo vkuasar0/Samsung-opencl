eb59beb73d6376ab2881f569fe5544d0b0606555
VarshaShripad <161583652+VarshaShripad@users.noreply.github.com>
2024-07-01 02:05:48 +0530

    Update adb_pull.sh

--- jni/adb_pull.sh
+++ jni/adb_pull.sh
@@ -5,5 +5,6 @@ adb pull /data/local/tmp/output_crop.png ../output
 adb pull /data/local/tmp/output_lanczos.png ../output
 adb pull /data/local/tmp/output_emboss.png ../output
 adb pull /data/local/tmp/output_gray_background.png ../output
+adb pull /data/local/tmp/output_image_cvt.png ../output
 
 adb pull /data/local/tmp/report.csv ../output
